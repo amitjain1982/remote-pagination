488b0fd7068ab2037d2c62ddf7dca740e372bcd2
Amit Jain <amit@middleware360.com>
2013-01-10 00:12:32 +0530

    Change version to 0.4.1

--- RemotePaginationGrailsPlugin.groovy
+++ RemotePaginationGrailsPlugin.groovy
@@ -1,6 +1,6 @@
 class RemotePaginationGrailsPlugin {
     // the plugin version
-    def version = "0.4"
+    def version = "0.4.1"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "2.0.3 > *"
     // the other plugins this plugin depends on
